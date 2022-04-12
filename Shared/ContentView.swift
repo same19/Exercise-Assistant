01cde45f7252347e204dbdb3ad42fa61bb44291d
Sam Engel <saengel123@gmail.com>
2022-04-11 20:44:12 -0600

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Sam Engel on 4/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Exercise_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exercise_AssistantApp.swift
+//  Shared
+//
+//  Created by Sam Engel on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Exercise_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
